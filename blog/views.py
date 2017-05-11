6d9275c75814de1e29a010c59c47d553825ec717
linyarui <2237091767@qq.com>
2017-05-11 13:44:11 +0800

    第四次提交

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .import models
 
 def index(request):
-    return render(request, 'blog/index.html')
+    article = models.Article.objects.get(pk=1)
+    return render(request, 'blog/index.html', {'article': article})
